0023cb38034fdebfec04115196634047d76893f8
aprianz042 <aprianz042@gmail.com>
2026-04-22 11:47:18 +0700

    ok

--- sidebar.py
+++ sidebar.py
@@ -4,7 +4,9 @@ def render_sidebar():
     with st.sidebar:
         st.markdown("## 📂 Menu")
 
+        st.page_link("pages/panduan.py", label="📘 Panduan")
+        
         st.page_link("pages/input1.py", label="🟢 Generator 1 Field")
         st.page_link("pages/input2.py", label="🟡 Generator 2 Field")
         st.page_link("pages/input3.py", label="🔵 Generator 3 Field")
-        st.page_link("pages/panduan.py", label="📘 Panduan")
+        
