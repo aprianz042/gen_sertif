2c5e5579ee9fab9bf4a93b99ed516a9b7a2dd6d6
aprianz042 <aprianz042@gmail.com>
2026-04-27 09:36:10 +0700

    27 april

--- pages/panduan.py
+++ pages/panduan.py
@@ -36,70 +36,17 @@ st.title("📘 Panduan Penggunaan Generator Sertifikat")
 # Template
 # =========================
 st.subheader("### Persiapan Template")
-st.markdown("Siapkan template yang ingin di-generate ")
+st.markdown("Siapkan template yang ingin di-generate, contoh : ")
 
 dataf = pd.DataFrame({
-    "header1": ["Budi Santoso", "Siti Aminah", "Andi Wijaya"],
-    "header2": ["1987654321", "1981234567", "1989988776"],
-    "header3": ["Analis Sistem", "Programmer", "Administrator"]
+    "nama": ["Budi Santoso", "Siti Aminah", "Andi Wijaya"],
+    "no id": ["1987654321", "1981234567", "1989988776"],
+    "jabatan": ["Analis Sistem", "Programmer", "Administrator"],
+    "instansi": ["Pemkab XXX", "Pemprov XXX", "Kementerian XXX"]
 })
 
 st.dataframe(dataf, use_container_width=True)
-st.markdown("header1 / header2 / header3 bebas diganti dengan nama kolom apapun, seperti nama, nip, jabatan, dll")
-
-st.divider()
-
-# =========================
-# Data
-# =========================
-st.subheader("### 📑 Persiapan Data (Bulk Data)")
-st.markdown("Digunakan untuk data yang banyak sekaligus")
-st.markdown("Data harus dalam bentuk CSV dengan contoh data di bawah ini: ")
-
-dataf = pd.DataFrame({
-    "header1": ["Budi Santoso", "Siti Aminah", "Andi Wijaya"],
-    "header2": ["1987654321", "1981234567", "1989988776"],
-    "header3": ["Analis Sistem", "Programmer", "Administrator"]
-})
-
-st.dataframe(dataf, use_container_width=True)
-st.markdown("header1 / header2 / header3 bebas diganti dengan nama kolom apapun, seperti nama, nip, jabatan, dll")
-
-st.divider()
-
-# =========================
-# 2 FIELD
-# =========================
-st.subheader("🟡 Contoh 2 Field (input1, input2)")
-st.image("template/INPUT2.png", caption="Contoh Sertifikat", use_container_width=True)
-st.markdown("### 📑 Contoh Format CSV")
-
-df2 = pd.DataFrame({
-    "input1": ["Budi Santoso", "Siti Aminah", "Andi Wijaya"],
-    "input2": ["1987654321", "1981234567", "1989988776"]
-})
-
-st.dataframe(df2, use_container_width=True)
-
-csv2 = load_csv_bytes("template/input2.csv")
-st.download_button("⬇️ Download Template", csv2, "input2.csv")
-
-st.divider()
-
-# =========================
-# 3 FIELD
-# =========================
-st.subheader("🔵 Contoh 3 Field (input1, input2, input3)")
-st.image("template/INPUT3.png", caption="Contoh Sertifikat", use_container_width=True)
-st.markdown("### 📑 Contoh Format CSV")
-
-df3 = pd.DataFrame({
-    "input1": ["Budi Santoso", "Siti Aminah", "Andi Wijaya"],
-    "input2": ["1987654321", "1981234567", "1989988776"],
-    "input3": ["Analis Sistem", "Programmer", "Administrator"]
-})
-
-st.dataframe(df3, use_container_width=True)
+st.markdown("nama / no id / jabatan / instansi bebas diganti dengan nama kolom apapun. Contoh template data dapat diunduh pada link di bawah")
 
 csv3 = load_csv_bytes("template/input3.csv")
 st.download_button("⬇️ Download Template", csv3, "input3.csv")
@@ -107,14 +54,36 @@ st.download_button("⬇️ Download Template", csv3, "input3.csv")
 st.divider()
 
 # =========================
+# 3 FIELD
+# =========================
+st.subheader("### Siapkan template sertifikat kosong (tanpa data peserta), contoh :")
+st.image("template/kosong.png")
+st.divider()
+
+# =========================
 # CATATAN
 # =========================
-st.markdown("### ⚠️ Catatan Penting")
-st.markdown("""
-- Gunakan format **CSV UTF-8**
-- Jangan ubah nama kolom (input1, input2, input3)
-- Data harus dimulai dari baris ke-2
-- Tidak boleh ada kolom kosong di header
-""")
+st.subheader("🔵 Panduan Penggunaan Aplikasi")
 
-st.success("Siap digunakan 🚀")
+st.success("Upload template sertifikat pada form input berikut")
+st.image("template/input_template.png")
+
+st.success("Untuk BULK DATA, upload data csv pada form input berikut")
+st.image("template/input_data.png")
+
+st.success("Untuk SINGLE DATA, tambahkan parameter dengan menekan tombol berikut")
+st.image("template/tombol_input.png")
+
+st.success("Pilih font dan warna font")
+st.image("template/pilih_font.png")
+
+st.success("Atur parameter text sesuai dengan jumlah header data pada Bulk Data atau jumlah input pada Single Data")
+st.image("template/param_font.png")
+
+st.success("Klik preview untuk mereview sertifikat, dan klik tombol generate untuk memproses sertifikat")
+st.image("template/tombol.png")
+
+st.success("Jika SINGLE DATA maka output-nya hanya 1 pdf, namun jika BULK DATA maka output-nya akan menjadi file ZIP")
+st.image("template/download_img.png")
+st.image("template/zip_img.png")
+
